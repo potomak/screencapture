8495c80b349215f45279f3dda1f2253d931fb3b5
Giovanni Cappellotto <potomak84@gmail.com>
2012-04-05 18:40:07 +0200

    Add screen capture command for Linux platforms

--- capture.sh
+++ capture.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+SCREENCAPTURE_CMD='unknown'
+
+if test "$(uname)" = 'Linux'; then
+  SCREENCAPTURE_CMD='import -window root'
+elif test "$(uname)" = 'Darwin'; then
+  SCREENCAPTURE_CMD='screencapture -t jpg -x'
+else
+  echo "This script doesn't support your OS type"
+  exit
+fi
+
 # screen captures
 while [ 1 ]; do
-  screencapture -t jpg -x ./images/`date +%s`.jpg
+  $SCREENCAPTURE_CMD ./images/`date +%s`.jpg
   echo "captured `date +%s`"
   sleep 4
 done
