c5ba981e53a8a6c3bbbf63e1144c78028a4251ef
Giovanni Cappellotto <potomak84@gmail.com>
2012-04-05 18:54:09 +0200

    Update timelaps video file name

--- make_movie.sh
+++ make_movie.sh
@@ -9,7 +9,7 @@ for i in $(ls -r -t ./images/*jpg); do
 done
 
 FRAME_RATE=24
-VIDEO_FILENAME=timelapse.mov
+VIDEO_FILENAME=timelapse_`date +%s`.mov
 
 # compile video
 ffmpeg -r $FRAME_RATE -i ./tmp/img%03d.jpg -b 15000k $VIDEO_FILENAME
