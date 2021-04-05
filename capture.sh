c7ffc46a9037934a491a5648fcd3a1b660b3ebfa
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2021-04-05 12:49:55 +0200

    Added --app and --period CL option
    With the --period option, the amount of seconds between consecutive captures can be specified. With the --app option, the screen will only be captured with a specified app in focus.

--- capture.sh
+++ capture.sh
@@ -2,6 +2,15 @@
 
 SCREENCAPTURE_CMD='unknown'
 
+#Parse the command line arguments
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+   fi
+  shift
+done
+
 if test "$(uname)" = 'Linux'; then
   SCREENCAPTURE_CMD='import -window root'
 elif test "$(uname)" = 'Darwin'; then
@@ -11,9 +20,25 @@ else
   exit 1
 fi
 
+PERIODI=4 #default to 4 seconds between captures. Can be changed with --period command line option
+if [ ! -z "${period}" ]
+    then
+      PERIODI="$period"
+fi
+
+if [ ! -z "${app}" ] && test "$(uname)" != 'Darwin'
+    then
+      echo 'The --app option is currently only available on macOS.'
+      exit 0
+fi
+
 # screen captures
 while [ 1 ]; do
-  $SCREENCAPTURE_CMD ./images/`date +%s`.jpg
-  echo "captured `date +%s`"
-  sleep 4
+  APPINFOCUS=`osascript -e 'tell application "System Events"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell'`
+  if [ "${APPINFOCUS}" == "${app}" ] || [ -z "${app}" ] #if --app command line option is provided, only capture when the specified app is in focus
+    then
+      $SCREENCAPTURE_CMD ./images/`date +%s`.jpg
+      echo "captured `date +%s`"
+  fi
+  sleep $PERIODI
 done
